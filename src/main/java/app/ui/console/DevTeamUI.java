9abe3fb924e9a1ac39b0d950c94ee3a964f3e231
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2021-04-29 22:27:17 +0100

    Atualization of the DevTeamUI [DevTeamUI]

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,11 +14,11 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 5 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t André Santos - 120534@isep.ipp.pt \n");
+        System.out.printf("\t Francisco Malheiro - 1201485@isep.ipp.pt \n");
+        System.out.printf("\t Guilherme Cunha - 1201506@isep.ipp.pt \n");
+        System.out.printf("\t João Martins - 1201539@isep.ipp.pt \n");
+        System.out.printf("\t Pedro Moreira - 1191526@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
