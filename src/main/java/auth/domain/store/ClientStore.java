35742875b295d24403d5d2e9708baf719fdc29d2
DESKTOP-LPMIHQM\Guilherme Cunha <1201506@isep.ipp.pt>
2021-05-02 15:00:08 +0100

    Add ClientStore class [US-03] [Implementation]

---
+++ src/main/java/auth/domain/store/ClientStore.java
@@ -0,0 +1,47 @@
+package auth.domain.store;
+
+import app.domain.model.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientStore {
+
+        private List<Client> clientList = new ArrayList();
+
+
+        public Client newClient(int nhsNumber, String name, int tinNumber, String birthDate, String sex, long phoneNumber, String email){
+            return new Client(nhsNumber, name, tinNumber, birthDate, sex, phoneNumber, email);
+        }
+
+        public boolean saveClient(Client ct){
+            if(validateClient(ct)){
+                addClient(ct);
+                return true;
+            }
+            return false;
+        }
+
+        public boolean addClient(Client ct)
+        {
+            if (ct != null) {
+                if (!validateClient(ct))
+                    return this.clientList.add(ct);
+            }
+            return false;
+        }
+
+        public boolean validateClient(Client ct) {
+            for (Client parClt : clientList){
+                if(parClt.equals(ct)){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Client> getClientList() {
+            return clientList;
+        }
+
+    }
