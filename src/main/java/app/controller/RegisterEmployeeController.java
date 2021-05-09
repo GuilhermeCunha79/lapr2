c5320c2601558b84b6289cd1d12de9dd23d5f8b5
torrespedro19 <1191526@isep.ipp.pt>
2021-05-09 23:22:55 +0100

    Introduction of some javadoc [UC-07] [Analysis].

--- src/main/java/app/controller/RegisterEmployeeController.java
+++ src/main/java/app/controller/RegisterEmployeeController.java
@@ -32,24 +32,50 @@ public class RegisterEmployeeController {
         this.authFacade = company.getAuthFacade();
     }
 
+    /**
+     * Method that checks if the employee is a specialist doctor or not
+     * @return if it is a specialist doctor (True or false)
+     */
     public boolean isSpecialistDoctor(String role){
         return role.equalsIgnoreCase(Constants.ROLE_SPECIALIST_DOCTOR);
     }
 
+    /**
+     * Method that creates a specialist doctor from the given information
+     * @param role of the employee
+     * @param name of the employee
+     * @param address of the employee
+     * @param phoneNumber of the employee
+     * @param email of the employee
+     * @param soc of the employee
+     * @return true or false
+     */
     public boolean createEmployee(String role, String name, String address, String phoneNumber, String email, String soc){
         this.emp = this.empStore.createEmployee(role, name, address, phoneNumber, email, soc);
         return this.empStore.validateEmployee(emp);
     }
 
+    /**
+     * Method that adds the given information in a user and after introduces it in the system
+     * @return if it was added (True or false)
+     */
     private boolean addUserToSystem(String name, String email, String role) {
         return CommonMethods.addUserToSystem(name, email, role, this.authFacade);
     }
 
+    /**
+     * Method that creates a specialist doctor from the given information
+     * @return if it was validates or not (True or false)
+     */
     public boolean createSpecialistDoctor(String role, String name, String address, String phoneNumber, String email, String soc, int indexNumber){
         this.emp = this.empStore.createSpecialistDoctor(role,name,address,phoneNumber,email,soc, indexNumber);
         return this.empStore.validateEmployee(emp);
     }
 
+    /**
+     * Method that asks the employee store to save the category being created
+     * @return if it was saved (True or false)
+     */
     public boolean saveEmployee() {
         if(this.empStore.saveEmployee(emp))
             return addUserToSystem(emp.getName(), emp.getEmail(), emp.getRole());
