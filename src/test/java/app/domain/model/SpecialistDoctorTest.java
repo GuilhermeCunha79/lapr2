5fdcd6f1f75acaa80bb858dc659bb4e91ef7b994
torrespedro19 <1191526@isep.ipp.pt>
2021-05-09 19:05:37 +0100

    Upgrade of the SpecialistDoctorTest EmployeeStoreTest.java [UC-07] [Implementation].

--- src/test/java/app/domain/model/SpecialistDoctorTest.java
+++ src/test/java/app/domain/model/SpecialistDoctorTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class SpecialistDoctorTest {
 
     SpecialistDoctor employee = new SpecialistDoctor(Constants.ROLE_SPECIALIST_DOCTOR, "Pedro", "Porto", "91111111221", "sfgsdfgsfdg@gmail.com", "1234",2);
-    SpecialistDoctor employee1 = new SpecialistDoctor(Constants.ROLE_SPECIALIST_DOCTOR, "Miguel", "Lisboa", "91111111221", "sfgsdfgsfdg@gmail.com", "1234",2);
+    SpecialistDoctor employee1 = new SpecialistDoctor(Constants.ROLE_SPECIALIST_DOCTOR, "Miguel", "Lisboa", "91131111221", "sfgsgsfdg@gmail.com", "4414",3);
 
     @Test
     public void getDoctorIndexNumber() {
@@ -29,7 +29,37 @@ public class SpecialistDoctorTest {
     }
 
     @Test
+    public void testEqualsIndex() {
+        assertTrue(employee.getDoctorIndexNumber()==employee.getDoctorIndexNumber());
+    }
+
+    @Test
+    public void testEqualsPhoneNumber() {
+        assertTrue(employee.getPhoneNumber().equals(employee.getPhoneNumber()));
+    }
+
+    @Test
+    public void testEqualsEmail() {
+        assertTrue(employee.getEmail().equals(employee.getEmail()));
+    }
+
+    @Test
     public void testEqualsFalse() {
         assertFalse(employee.equals(employee1));
+    }
+
+    @Test
+    public void testEqualsIndexFalse() {
+        assertFalse(employee.getDoctorIndexNumber()==employee1.getDoctorIndexNumber());
+    }
+
+    @Test
+    public void testEqualsPhoneNumberFalse() {
+        assertFalse(employee.getPhoneNumber().equals(employee1.getPhoneNumber()));
+    }
+
+    @Test
+    public void testEqualsEmailFalse() {
+        assertFalse(employee.getEmail().equals(employee1.getEmail()));
     }
 }
