78a21e52aedafcf930c8b17258d6e1e26c738318
torrespedro19 <1191526@isep.ipp.pt>
2021-05-26 10:26:44 +0100

    Update of the class TestParameterResult of UC [UC-12] [Implementation]

--- src/main/java/app/domain/model/TestParameterResult.java
+++ src/main/java/app/domain/model/TestParameterResult.java
@@ -1,6 +1,6 @@
 package app.domain.model;
 
-import app.domain.shared.DateTime;
+import   app.domain.shared.DateTime;
 import org.apache.commons.lang3.NotImplementedException;
 
 public class TestParameterResult {
@@ -8,9 +8,24 @@ public class TestParameterResult {
     private DateTime createdAt;
     private String value;
     private String metric;
+    private Parameter testParameter;
+    private String parameterResult;
+
+    public TestParameterResult(Parameter parameter, String result) {
+        setParameter(parameter);
+        setResult(result);
+    }
+
+    public void setParameter(Parameter parameter){
+        this.testParameter = parameter;
+    }
+
+    public void setResult(String result){
+        this.parameterResult = result;
+    }
 
     @Override
     public String toString() {
-        throw new NotImplementedException("Method not implemented yet");
+        return String.format("Test Parameter of %s has the following result :", testParameter, parameterResult);
     }
 }
