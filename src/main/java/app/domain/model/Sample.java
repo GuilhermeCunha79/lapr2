f121646877d6c9d15eac45ab70ac2fd74f2c65b2
1201485 <1201485@isep.ipp.pt>
2021-05-29 18:12:49 +0100

    [UC5] Uptate of code of the US

--- src/main/java/app/domain/model/Sample.java
+++ src/main/java/app/domain/model/Sample.java
@@ -1,24 +1,34 @@
 package app.domain.model;
 
-import net.sourceforge.barbecue.Barcode;
-import net.sourceforge.barbecue.BarcodeException;
-import net.sourceforge.barbecue.BarcodeFactory;
-import net.sourceforge.barbecue.output.OutputException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Sample {
-    private List<Sample> sampleList = new ArrayList<>();
+    private List<Sample> sampleList ;
 
+    /**
+     * creates a list of class sample list
+     * @param sampleList
+     */
     public Sample(ArrayList sampleList) {
         this.sampleList = new ArrayList<>(sampleList);
     }
 
+    /**
+     * method to get the sample list
+     * @return sampleList
+     */
+    public List<Sample> getSampleList() { return sampleList; }
 
-
-
-
+    /**
+     * to String method to show the
+     * @return a String
+     */
+    @Override
+    public String toString() {
+        return "Sample:"+sampleList;
+    }
 }
 
