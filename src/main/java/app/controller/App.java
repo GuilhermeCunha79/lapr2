6d9749c4035dc7f79654551430e735f7bf60679b
An_dreOliveira <1201534@isep.ipp.pt>
2021-05-30 23:27:05 +0100

    Last Sprint C Commit with project build

--- src/main/java/app/controller/App.java
+++ src/main/java/app/controller/App.java
@@ -8,7 +8,6 @@ import auth.UserSession;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Properties;
 
 /**
@@ -82,16 +81,7 @@ public class App {
         this.authFacade.addUserRole(Constants.ROLE_CLINICAL_CHEMISTRY_TECHNOLOGIST,Constants.ROLE_CLINICAL_CHEMISTRY_TECHNOLOGIST);
         this.authFacade.addUserRole(Constants.ROLE_MEDICAL_LAB_TECHNICIAN,Constants.ROLE_MEDICAL_LAB_TECHNICIAN);
 
-
         this.authFacade.addUserWithRole("Main Administrator", "admin@lei.sem2.pt", "123456",Constants.ROLE_ADMIN);
-        this.authFacade.addUserWithRole("Receptionist", "recep@lei.sem2.pt", "123456",Constants.ROLE_RECEPTIONIST);
-        this.authFacade.addUserWithRole("Clinical chemistry technologist", "cct@lei.sem2.pt", "123456",Constants.ROLE_CLINICAL_CHEMISTRY_TECHNOLOGIST);
-
-        this.getCompany().getParameterCategoryStore().saveParameterCategory(new ParameterCategory("12345", "Blood"));
-        this.getCompany().getParameterStore().saveParameter(new Parameter("WBC00", "WBC", "White blood count", new ParameterCategory("12345", "Blood")));
-        this.getCompany().getParameterStore().saveParameter(new Parameter("RBC00", "RBC", "Red blood count", new ParameterCategory("12345", "Blood")));
-        this.getCompany().getTypeOfTestStore().saveTypeOfTest(new TypeOfTest("09090", "blood test", "seringe", new ParameterCategory("12345", "Blood")));
-        this.getCompany().getClinicalAnalysisLaboratoryStore().saveClinicalAnalysisLaboratory(new ClinicalAnalysisLaboratory("l0001", "lab 1", "street 1", "90909090909", "8978787878"));
     }
 
     // Extracted from https://www.javaworld.com/article/2073352/core-java/core-java-simply-singleton.html?page=2
--- src/main/java/app/ui/console/ValidationUI.java
+++ src/main/java/app/ui/console/ValidationUI.java
@@ -3,6 +3,7 @@ package app.ui.console;
 import app.controller.ValidationController;
 import app.ui.console.utils.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,18 +13,53 @@ public class ValidationUI implements Runnable {
     @Override
     public void run() {
         boolean repeat;
-        do{
-            repeat = validationProcess();
-        }while(repeat && Objects.requireNonNull(Utils.readLineFromConsole("Do you want to do another validation? (Y/N)")).equalsIgnoreCase("y"));
+        int validationProcess = validationMethod();
+        if (validationProcess == 0) {
+            validateAllTests();
+        } else if (validationProcess == 1) {
+            do {
+                repeat = validationProcess();
+            } while (repeat && Objects.requireNonNull(Utils.readLineFromConsole("Do you want to do another validation? (Y/N)")).equalsIgnoreCase("y"));
+        } else {
+            System.out.println("INFO: Invalid option");
+        }
+    }
+
+    private void validateAllTests() {
+        System.out.println("These tests are going to be validated:");
+        List<String> testList = ctrl.getTestWithoutValidation();
+        try {
+            for(String test : testList){
+                System.out.println(test);
+            }
+            if (Objects.requireNonNull(Utils.readLineFromConsole("Confirm validation? (Y/N)")).equalsIgnoreCase("y")) {
+                if (ctrl.doValidation(testList))
+                    System.out.println("INFO: All tests were successfully validated");
+                else
+                    System.out.println("INFO: Something wrong happen");
+            } else {
+                System.out.println("INFO: Validation aborted");
+            }
+        }catch(Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    private int validationMethod() {
+        List<String> options = new ArrayList<>();
+        options.add("Validate all tests");
+        options.add("Validate a subset of tests");
+        return Utils.showAndSelectIndex(options, "Select one of the following options:");
     }
 
     /**
      * This method is responsible to guide the user through the test selection and report creation process
+     *
      * @return false if its not possible to write another report due to not having more tests
      * needing reports, return true when there are tests still needing reports
      */
     private boolean validationProcess() {
-        while (true){
+        while (true) {
             try {
                 List<String> rtvListDto = ctrl.getTestWithoutValidation();
                 if (rtvListDto != null) {
@@ -32,7 +68,7 @@ public class ValidationUI implements Runnable {
                     System.out.println(results);
                     System.out.printf("Confirm the tests to validate: %n%s%n", rtvListDto.get(option));
                     if (Objects.requireNonNull(Utils.readLineFromConsole("Y or N:")).equalsIgnoreCase("y")) {
-                        if(ctrl.saveValidation())
+                        if (ctrl.saveValidation())
                             System.out.println("Validation Saved!");
                         return true;
                     }
