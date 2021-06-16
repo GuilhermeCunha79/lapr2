a634c6f39f97418909e90c4f9892d4a32ecd2a54
1201485 <1201485@isep.ipp.pt>
2021-06-16 15:45:32 +0100

    US18 [Implementation] Added Simple regression model class

--- src/main/java/app/controller/SendNHSReportControler.java
+++ src/main/java/app/controller/SendNHSReportControler.java
@@ -6,7 +6,9 @@ import app.domain.model.SimpleRegressionModel;
 import app.domain.store.TestStore;
 import app.mappers.TestListMapper;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class SendNHSReportControler {
 
@@ -29,16 +31,36 @@ public class SendNHSReportControler {
         this.testStore = company.getTestStore();
     }
 
+
+
+
     /**
      * This method returns a list with the main information about each test available without a report
      * @return a list of Strings holding the data for each test
      */
-    public List<String> getFinalisedTest(){
+    public List<ClinicalTest> getFinalisedTest(){
         List<ClinicalTest> FinalisedTest = testStore.getTestFinalizated();
         if(!FinalisedTest.isEmpty()) {
             return TestListMapper.toDto(FinalisedTest);
         }
-        return null;
+        return FinalisedTest;
+    }
+
+
+
+    public static List<Date> getDaysBetweenDates(Date startDate, Date endDate )
+    {
+        List<Date> dateList= new ArrayList<>();
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(startDate);
+        while (cal.getTime().before(endDate))
+        {
+            Date result = cal.getTime();
+            dateList.add(result);
+            cal.add(Calendar.DATE, 1);
+        }
+        return dateList;
     }
 
 
